02200193bc93bcc49aea8fe69a66e8efd21854a7
cogito96 <bujnolukasz@gmail.com>
2017-12-17 00:55:07 +0100

    funkcjonalny przycisk

--- app/src/main/java/com/example/aa/mydatabase/CreateUser.java
+++ app/src/main/java/com/example/aa/mydatabase/CreateUser.java
@@ -1,8 +1,17 @@
 package com.example.aa.mydatabase;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by aa on 16.12.2017.
  */
 
-public class CreateUser {
+public class CreateUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+    }
 }
